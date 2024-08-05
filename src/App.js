a878d0376546155832e6f776fdb6ea24ec2d6520
bayazidsustami <bayazidsustamy@gmail.com>
2024-08-05 22:51:23 +0700

    feat : complete tic tac toe tutor

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import './Title.js'
-import Title from './Title.js';
+import Game from './components/Game.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Title/>
-      </header>
-    </div>
+    <Game/>
   );
 }
 
